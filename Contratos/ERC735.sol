8001ed6516b714f47e84242d01178400c6e00d03
Iker Macaya Faber <42570380+Maciker@users.noreply.github.com>
2021-03-21 17:10:03 +0100

    Commit!

---
+++ Contratos/ERC735.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.22;
+
+// **Warning!** This file is a protoype version of our work around ERC 725.
+// This file is now out of date and **should not be used**.
+// Our current identity contracts are here:
+// https://github.com/OriginProtocol/origin/tree/master/origin-contracts/contracts/identity
+
+contract ERC735 {
+
+    event ClaimRequested(uint256 indexed claimRequestId, uint256 indexed claimType, uint256 scheme, address indexed issuer, bytes signature, bytes data, string uri);    event ClaimAdded(bytes32 indexed claimId, uint256 indexed claimType, address indexed issuer, uint256 signatureType, bytes32 signature, bytes claim, string uri);
+    event ClaimAdded(bytes32 indexed claimId, uint256 indexed claimType, uint256 scheme, address indexed issuer, bytes signature, bytes data, string uri);
+    event ClaimRemoved(bytes32 indexed claimId, uint256 indexed claimType, uint256 scheme, address indexed issuer, bytes signature, bytes data, string uri);
+    event ClaimChanged(bytes32 indexed claimId, uint256 indexed claimType, uint256 scheme, address indexed issuer, bytes signature, bytes data, string uri);
+
+    struct Claim {
+        uint256 claimType;
+        uint256 scheme;
+        address issuer; // msg.sender
+        bytes signature; // this.address + claimType + data
+        bytes data;
+        string uri;
+    }
+
+    function getClaim(bytes32 _claimId) public constant returns(uint256 claimType, uint256 scheme, address issuer, bytes signature, bytes data, string uri);
+    function getClaimIdsByType(uint256 _claimType) public constant returns(bytes32[] claimIds);
+    function addClaim(uint256 _claimType, uint256 _scheme, address issuer, bytes _signature, bytes _data, string _uri) public returns (bytes32 claimRequestId);
+    function removeClaim(bytes32 _claimId) public returns (bool success);
+}
